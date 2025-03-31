c553cc76a858e5469313ae8d057340fa129f7ec5
Kalinin Nikolai <forizual@gmail.com>
2025-03-31 22:28:05 +0100

    add tone.js

--- src/pages/MainPage/MainPage.svelte
+++ src/pages/MainPage/MainPage.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from 'svelte'
+    import * as Tone from 'tone'
     import dayjs from 'dayjs'
 
     const now = dayjs()
@@ -17,8 +18,15 @@
 
         return () => clearInterval(interval)
     })
+
+    const handleStartTone = async () => {
+        const synth = new Tone.Synth().toDestination()
+        synth.triggerAttackRelease('C4', '8n')
+    }
 </script>
 
 <h1>Kāla</h1>
 
 <h2>{`${hours}:${minutes}:${seconds}`}</h2>
+
+<button on:click={handleStartTone}>Start</button>
